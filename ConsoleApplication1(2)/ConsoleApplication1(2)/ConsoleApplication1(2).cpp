fce5d900313571d397a2cb019a7bc39e02b8dfdb
Aleksander999 <59916045+Aleksander999@users.noreply.github.com>
2020-01-17 18:26:33 +0300

    Add files via upload

---
+++ ConsoleApplication1(2)/ConsoleApplication1(2)/ConsoleApplication1(2).cpp
@@ -0,0 +1,16 @@
+﻿// ConsoleApplication1(2).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <clocale>
+
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout << "Результаты вычисления выражения 2+2* =" << 2 + 2 * 2 << endl;
+	return 0;
+
+}
+
